f258e0cf716ab3ecefd733b2ea7859cdc7bb2018
Nie Ridwan Susanto <xa18ridwan@gmail.com>
2024-05-11 02:53:17 +0700

    feat: final touch & first content

---
+++ src/pages/_partials/HomeContent.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import BaseCard from "src/components/BaseCard.svelte";
+</script>
+
+<BaseCard className="p-4 flex flex-col items-center">
+  <h1 class="text-4xl font-bold text-center block">Welcome!</h1>
+  <article class="pt-4 block w-full max-w-4xl">
+    <hr />
+    <section class="pt-4">
+      Hello, Welcome to my atelier. I'm a knowledge-devouring creature, whose
+      main quest is to devour all software development stuff. I believe that
+      knowledge itself is infinite, and thus it'll be a neverending journey for
+      me.
+      <br />
+      As a side quest, I also play games, such as Honkai Impact 3rd, Honkai Star
+      Rail, Genshin Impact, and Granblue Fantasy. When consumed in the correct amount,
+      they will increase my motivation to continue my main quest.
+    </section>
+    <section class="pt-4">
+      From here onwards, I'll do some experiments and share the results at the
+      blog section. Also, I'll post my existing knowledge, as opinionated tips &
+      recommendations around software development stuff. This will also help me
+      remember the journey I've done, maybe sometime in the future I'll have a
+      hard time remembering it. Hope this will help people who come across this
+      site to scroll and read.
+    </section>
+    <section class="pt-4">
+      At the moment, I'm mostly proficient in mobile development-related stuff,
+      especially Android stuff, then web development in second place. I'd like
+      to balance my knowledge later, and hope game development would be my top
+      three.
+    </section>
+    <section class="pt-4">
+      Note for myself: If you ever lost your way and remember this site
+      accidentally, remember, you have did your best, and continue moving
+      forward.
+    </section>
+  </article>
+</BaseCard>
