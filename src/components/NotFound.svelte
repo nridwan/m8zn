6454322ac368ed2191410b31707944816b8d1d6e
Nie Ridwan Susanto <xa18ridwan@gmail.com>
2024-07-01 01:22:59 +0700

    fix: compile error when use bun, and retry compile using bun

---
+++ src/components/NotFound.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import BaseCard from "./BaseCard.svelte";
+</script>
+
+<BaseCard
+  className="px-4 py-8 flex flex-col items-center justify-center text-2xl font-semibold"
+>
+  Not Found
+</BaseCard>
